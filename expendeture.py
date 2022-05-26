90aa6df0592f976e8f0ef10dc8ce4530e69bdf08
me <ajmalc6705@gmail.com>
2022-05-26 14:01:48 +0400

    [main]: new expendeture

--- expendeture.py
+++ expendeture.py
@@ -1,15 +1,18 @@
 exp = -1
+list= []
 total = 0
 max_exp = 0
 min_exp = 0
 while exp!=0:
     exp = int(input("Enter Expendeture "))
+    list.append(exp)
     total = total + exp
     if exp>max_exp:
         max_exp=exp
     else:
         min_exp=exp
 
+print(list)
 print(total)
 print(max_exp)
 print(min_exp)
